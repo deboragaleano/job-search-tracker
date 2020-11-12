aec4b267c690a951a9ab3f0c89eaa31ee9f545be
deboragaleano <dgaleanoservian@gmail.com>
2020-11-12 17:31:42 +0100

    starter code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import {makeStyles} from '@material-ui/styles';
 import Navbar from './components/Navbar'; 
+import { Container } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    fontFamily: 'Abel',
+    alignItems: 'center',
+    textAlign: 'center', 
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <Navbar /> 
-    </div>
+    <Container className={classes.root}>
+        <Navbar /> 
+    </Container> 
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { Container, Button } from '@material-ui/core';
 
-export default function Navbar() {
+const NavButton = withStyles((theme) => ({
+    root: {
+      color: '#D2691E',
+      fontWeight: 'bolder', 
+      fontFamily: 'Abel',
+      fontSize: '15px', 
+      border: '1px solid #D2691E', 
+      letterSpacing: '1px',
+      margin: '20px', 
+      transition:'all 0.5s ease',
+      '&:hover': {
+        color: '#D2691E',
+      },
+    },
+  }))(Button);
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        margin: '30px 0 50px 0',
+    }, 
+    active: {
+        fontWeight: 'bolder',
+        color: 'white', 
+        backgroundColor: '#D2691E'
+    }
+}))
+
+function Navbar() {
+    const classes = useStyles(); 
+
     return (
-        <div>
-            <p>Navbar</p>
-        </div>
+        <Container className={classes.root}>
+            <h1>JOB SEARCH TRACKER</h1>
+            <NavButton>LIST</NavButton>
+            <NavButton>ADD NEW</NavButton>
+        </Container>
     )
 }
+
+export default Navbar
