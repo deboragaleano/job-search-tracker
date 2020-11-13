83ba3813601c6c022d47d559f5970a5855c6c3e8
deboragaleano <dgaleanoservian@gmail.com>
2020-11-13 22:18:04 +0100

    move data to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
 import {makeStyles} from '@material-ui/styles';
 import Navbar from './components/Navbar'; 
-// import List from './components/List'; 
+import List from './components/List'; 
 // import AddItem from './components/AddItem';
 import { Container } from '@material-ui/core';
+
+const applications = [
+  {
+      company: 'Some Company',
+      position: 'FrontEnd', 
+      link: 'https://i.ibb.co/54CHvmW/workout-app.png',
+      date: 'Nov 7'
+  },
+  {
+      company: 'Yes Company',
+      position: 'Customer Support', 
+      link: 'https://i.ibb.co/54CHvmW/workout-app.png',
+      date: 'Nov 7'
+  },
+  {
+      company: 'Company',
+      position: 'Backend', 
+      link: 'https://i.ibb.co/54CHvmW/workout-app.png',
+      date: 'Nov 10'
+  },
+]
 
 const useStyles = makeStyles({
   root: {
@@ -19,7 +40,7 @@ function App() {
   return (
     <Container className={classes.root}>
         <Navbar /> 
-        {/* <List />  */}
+        <List applications={applications}/> 
         {/* <AddItem />  */}
     </Container> 
   );
