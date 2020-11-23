a1412fac30f34789d051eee0ecaa6b1b6b6b9c9c
deboragaleano <dgaleanoservian@gmail.com>
2020-11-23 21:48:47 +0100

    add navbar again

--- src/App.js
+++ src/App.js
@@ -1,50 +1,35 @@
 import "./App.css";
-import appService from './services/applications';
+import appService from "./services/applications";
 import { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/styles";
 import List from "./components/List";
 import AddItem from "./components/AddItem";
 import Search from "./components/Search";
 import { Container } from "@material-ui/core";
-
-const useStyles = makeStyles({
-  root: {
-    fontFamily: "Abel",
-    alignItems: "center",
-    textAlign: "center",
-  },
-});
+import Navbar from "./components/Navbar";
 
 function App() {
   const [applications, setApplications] = useState([]);
   const [search, setSearch] = useState("");
-  const classes = useStyles();
 
   useEffect(() => {
-    appService
-      .getItems() 
-      .then(resp => {
-        setApplications(resp)
-      })
-  }, []); 
+    appService.getItems().then((resp) => {
+      setApplications(resp);
+    });
+  }, []);
 
   const addItem = (newItem) => {
-    appService
-      .create(newItem)
-      .then(returnedItem => {
-        setApplications([...applications, returnedItem]);
-      })
+    appService.create(newItem).then((returnedItem) => {
+      setApplications([...applications, returnedItem]);
+    });
   };
 
   const removeItem = (id) => {
     const toDelete = applications.find((app) => app.id === id);
     const isOk = window.confirm(`Delete ${toDelete.company}?`);
     if (isOk) {
-      appService
-        .remove(id)
-        .then(resp => {
-          setApplications(applications.filter(app => app.id !== id));
-        })
+      appService.remove(id).then((resp) => {
+        setApplications(applications.filter((app) => app.id !== id));
+      });
     }
   };
 
@@ -56,14 +41,16 @@ function App() {
         );
 
   return (
-    <Container className={classes.root}>
-      <h1>JOB SEARCH TRACKER</h1>
-      <Search value={search} onChange={(e) => setSearch(e.target.value)} />
-      <List applications={filteredItems} remove={removeItem} />
-      <AddItem addItem={addItem} />
-    </Container>
+    <>
+      <Navbar />
+      <Container>
+        <h1>JOB SEARCH TRACKER</h1>
+        <Search value={search} onChange={(e) => setSearch(e.target.value)} />
+        <List applications={filteredItems} remove={removeItem} />
+        <AddItem addItem={addItem} />
+      </Container>
+    </>
   );
 }
 
 export default App;
-
